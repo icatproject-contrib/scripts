c72a23d4fe0f4d8bf8e2e4e4138f7ba3008b01bd
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2022-07-12 18:12:44 +0200

    Add a command line switch --fix-values to fix wrong fileCount and fileSize values

--- scripts/check-sizes.py
+++ scripts/check-sizes.py
@@ -5,7 +5,9 @@ This script checks all Datasets and Investigations and verifies the
 values of the fileCount and fileSize attributes respectively.
 
 The script needs to be run by an ICAT user having read access to all
-Investigations, Datasets, and Datafiles.
+Investigations, Datasets, and Datafiles.  If you use the --fix-values
+command line switch, this user must furthermore have update permission
+for Investigations and Datasets.
 
 Note that the size attributes are expected to be in the next major
 release of icat.server 5.0, but are not yet present in any release
@@ -17,7 +19,6 @@ may differ.
 The script requires python-icat >= 0.18.0.
 
 .. __: https://github.com/icatproject/icat.server/pull/256
-
 """
 
 from distutils.version import StrictVersion as Version
@@ -28,10 +29,11 @@ from icat.query import Query
 
 log = logging.getLogger(__name__)
 
-def check_dataset(inv, ds):
+def check_dataset(inv, ds, fixvalues=False):
     client = ds.client
     ds_name = "Dataset(%s / %s / %s)" % (inv.name, inv.visitId, ds.name)
     log.debug("Considering %s", ds_name)
+    need_fix = False
     fileCount_query = Query(client, "Datafile", conditions={
         "dataset.id": "= %d" % ds.id
     }, aggregate="COUNT")
@@ -42,9 +44,11 @@ def check_dataset(inv, ds):
         if ds.fileCount:
             log.warn("%s: fileCount is wrong: %d versus %d",
                      ds_name, ds.fileCount, 0)
+            need_fix = True
         if ds.fileSize:
             log.warn("%s: fileSize is wrong: %d versus %d",
                      ds_name, ds.fileSize, 0)
+            need_fix = True
     else:
         fileSize_query = Query(client, "Datafile", conditions={
             "dataset.id": "= %d" % ds.id
@@ -52,27 +56,38 @@ def check_dataset(inv, ds):
         fileSize = client.assertedSearch(fileSize_query)[0]
         if ds.fileCount is None:
             log.warn("%s: fileCount is not set", ds_name)
+            need_fix = True
         elif ds.fileCount != fileCount:
             log.warn("%s: fileCount is wrong: %d versus %d",
                      ds_name, ds.fileCount, fileCount)
+            need_fix = True
         if ds.fileSize is None:
             log.warn("%s: fileSize is not set", ds_name)
+            need_fix = True
         elif ds.fileSize != fileSize:
             log.warn("%s: fileSize is wrong: %d versus %d",
                      ds_name, ds.fileSize, fileSize)
+            need_fix = True
+    if fixvalues and need_fix:
+        ds.fileCount = fileCount
+        ds.fileSize = fileSize
+        ds.update()
     return fileCount, fileSize
 
-def check_investigation(inv):
+def check_investigation(inv, fixvalues=False):
     client = inv.client
     inv_name = "Investigation(%s / %s)" % (inv.name, inv.visitId)
     log.debug("Check sizes in %s", inv_name)
     fileCount = 0
     fileSize = 0
+    need_fix = False
     ds_select = Query(client, "Dataset", conditions={
         "investigation.id": "= %d" % inv.id
     })
+    if fixvalues:
+        ds_select.addIncludes("1")
     for ds in client.searchChunked(ds_select):
-        ds_c, ds_s = check_dataset(inv, ds)
+        ds_c, ds_s = check_dataset(inv, ds, fixvalues)
         fileCount += ds_c
         fileSize += ds_s
     if not fileCount:
@@ -80,20 +95,30 @@ def check_investigation(inv):
         if inv.fileCount:
             log.warn("%s: fileCount is wrong: %d versus %d",
                      inv_name, inv.fileCount, 0)
+            need_fix = True
         if inv.fileSize:
             log.warn("%s: fileSize is wrong: %d versus %d",
                      inv_name, inv.fileSize, 0)
+            need_fix = True
     else:
         if inv.fileCount is None:
             log.warn("%s: fileCount is not set", inv_name)
+            need_fix = True
         elif inv.fileCount != fileCount:
             log.warn("%s: fileCount is wrong: %d versus %d",
                      inv_name, inv.fileCount, fileCount)
+            need_fix = True
         if inv.fileSize is None:
             log.warn("%s: fileSize is not set", inv_name)
+            need_fix = True
         elif inv.fileSize != fileSize:
             log.warn("%s: fileSize is wrong: %d versus %d",
                      inv_name, inv.fileSize, fileSize)
+            need_fix = True
+    if fixvalues and need_fix:
+        inv.fileCount = fileCount
+        inv.fileSize = fileSize
+        inv.update()
 
 def main():
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
@@ -101,6 +126,9 @@ def main():
     logging.getLogger('suds').setLevel(logging.WARN)
 
     config = icat.config.Config(ids=False)
+    config.add_variable('fixvalues', ("--fix-values",),
+                        dict(help="fix wrong fileCount and fileSize values"),
+                        type=icat.config.flag, default=False)
     client, conf = config.getconfig()
     client.login(conf.auth, conf.credentials)
 
@@ -115,8 +143,11 @@ def main():
         raise RuntimeError("This ICAT server does not support the size "
                            "attributes in Datasets and Investigations")
 
-    for inv in client.searchChunked(Query(client, "Investigation")):
-        check_investigation(inv)
+    inv_query = Query(client, "Investigation")
+    if conf.fixvalues:
+        inv_query.addIncludes("1")
+    for inv in client.searchChunked(inv_query):
+        check_investigation(inv, conf.fixvalues)
 
 if __name__ == "__main__":
     main()
